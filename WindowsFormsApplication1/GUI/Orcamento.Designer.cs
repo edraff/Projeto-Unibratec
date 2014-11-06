51f422ccc012a8d5cc92df01afc145fd5ba8a3c2
Bruno Cabral <edraff@hotmail.com>
2014-11-06 19:39:17 -0300

    ajshdasd

--- WindowsFormsApplication1/GUI/Orcamento.Designer.cs
+++ WindowsFormsApplication1/GUI/Orcamento.Designer.cs
@@ -34,7 +34,9 @@
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.listView1 = new System.Windows.Forms.ListView();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.listView2 = new System.Windows.Forms.ListView();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -78,20 +80,40 @@
             this.richTextBox1.TabIndex = 3;
             this.richTextBox1.Text = "";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(411, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // listView2
             // 
-            this.listView2.Location = new System.Drawing.Point(472, 61);
+            this.listView2.Location = new System.Drawing.Point(501, 61);
             this.listView2.Name = "listView2";
-            this.listView2.Size = new System.Drawing.Size(416, 181);
-            this.listView2.TabIndex = 4;
+            this.listView2.Size = new System.Drawing.Size(387, 181);
+            this.listView2.TabIndex = 6;
             this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(411, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Orcamento
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(900, 400);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.listView2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.textBox1);
@@ -112,7 +134,9 @@
         private System.Windows.Forms.ToolTip toolTip1;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Button button2;
 
     }
 }
