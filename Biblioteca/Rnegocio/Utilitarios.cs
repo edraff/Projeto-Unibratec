2189edcd7f9988dc7bc55663c7a10105ba88c143
Bruno Cabral <edraff@hotmail.com>
2014-11-08 01:13:15 -0300

    Mudei o nome da pasta em que o utilitario estava

---
+++ Biblioteca/Rnegocio/Utilitarios.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Biblioteca.Rnegocio
+{
+    public class Utilitarios
+    {
+        public Utilitarios() { }
+
+        public string gerarSenha(string _senha)
+        {
+            StringBuilder senha = new StringBuilder();
+
+            MD5 md5 = MD5.Create();
+            byte[] entrada = Encoding.ASCII.GetBytes("//" + _senha);
+            byte[] hash = md5.ComputeHash(entrada);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                senha.Append(hash[i].ToString("X2"));
+            }
+            return senha.ToString();
+        }
+    }
+}
